7c9c22f5217a95129809fee9a644e6350b45471c
Anatoly Gorshkov <tolyaig@gmail.com>
2022-06-04 19:09:48 +0200

    Added get_object_or_404() Method.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -25,7 +25,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     #Make sure the topic belongs to the current user.
     check_topic_owner(request, topic)
 
